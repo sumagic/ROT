4dcb428f929c18bca028d1ae03ebfcb6ca20d997
sumagic <kangxuzju@163.com>
2020-09-17 00:46:32 +0800

    add todo prototxt parse

--- inc/proto/parser.hpp
+++ inc/proto/parser.hpp
@@ -1,12 +1,22 @@
 #ifndef __ROT_PARSER_HPP__
 #define __ROT_PARSER_HPP__
 
+/*
+void ReadNetParamsFromTextFileOrDie(const string& param_file,
+                                    NetParameter* param) {
+  CHECK(ReadProtoFromTextFile(param_file, param))
+      << "Failed to parse NetParameter file: " << param_file;
+  UpgradeNetAsNeeded(param_file, param);
+}
+*/
+
+
 namespace rot
 {
     class PrototxtParser
     {
         public:
-            
+
     }
 } // namespace rot
 
