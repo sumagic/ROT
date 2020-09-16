4d4e58a993efc39ff3551f8a840833f16f22fbc5
sumagic <kangxuzju@163.com>
2020-09-17 00:22:28 +0800

    编译OK

--- src/tool/rot-server.cc
+++ src/tool/rot-server.cc
@@ -1,6 +1,17 @@
 #include <glog/logging.h>
+#include <gflags/gflags.h>
+
+
 
 int main(int argc, char** argv)
 {
+    // glog初始化
+    google::InitGoogleLogging(argv[0]);
+    google::SetLogFilenameExtension("glog_");
+    google::InstallFailureSignalHandler();
+    google::SetLogDestination(google::INFO, "./glog/");
+
+    LOG(INFO) << "server start";
+
     return 0;
 }
