5f126dece747558fe1d8215e15e73ca1ffadf420
sumagic <kangxuzju@163.com>
2020-09-17 00:02:33 +0800

    开始构建rot

--- build.sh
+++ build.sh
@@ -50,23 +50,68 @@ if [ ! -d ${THIRD_DIR} ];then
     log_info "create dir ${THIRD_DIR} success"
 fi
 
-# git clone https://github.com/gflags/gflags.git
-GFLAGS_DIR=${THIRD_DIR}/gflags
-if [ ! -d ${GFLAGS_DIR} ];then
-    cd ${THIRD_DIR}
-    git clone https://github.com/gflags/gflags.git
-fi
-cd ${GFLAGS_DIR}
-git submodule update --init
+# # git clone https://github.com/gflags/gflags.git
+# GFLAGS_DIR=${THIRD_DIR}/gflags
+# if [ ! -d ${GFLAGS_DIR} ];then
+#     cd ${THIRD_DIR}
+#     git clone https://github.com/gflags/gflags.git
+# fi
+# cd ${GFLAGS_DIR}
+# git submodule update --init
 
-# git clone https://github.com/google/glog.git
-GLOG_DIR=${THIRD_DIR}/glog
-if [ ! -d ${GLOG_DIR} ];then
-    cd ${THIRD_DIR}
-    git clone https://github.com/google/glog.git
+# # git clone https://github.com/google/glog.git
+# GLOG_DIR=${THIRD_DIR}/glog
+# if [ ! -d ${GLOG_DIR} ];then
+#     cd ${THIRD_DIR}
+#     git clone https://github.com/google/glog.git
+# fi
+# cd ${GLOG_DIR}
+# git submodule update --init
+
+# # create build
+BUILD_X86_DIR=${PROJECT_DIR}/build/x86
+# if [ ! -d ${BUILD_X86_DIR} ];then
+#     mkdir -p ${BUILD_X86_DIR}
+#     log_warn "create ${BUILD_X86_DIR} success"
+# fi
+# BUILD_AARCH64_DIR=${PROJECT_DIR}/build/aarch64
+# if [ ! -d ${BUILD_AARCH64_DIR} ];then
+#     mkdir -p ${BUILD_AARCH64_DIR}
+#     log_warn "create ${BUILD_AARCH64_DIR} success"
+# fi
+
+# # X86编译安装glog
+# cd ${GLOG_DIR}
+# ./autogen.sh
+# CONFIGURE_X86_GLOG=${BUILD_X86_DIR}/glog
+# if [ ! -d ${CONFIGURE_X86_GLOG} ];then
+#     mkdir -p ${CONFIGURE_X86_GLOG}
+# fi
+# cd ${CONFIGURE_X86_GLOG}
+# ${GLOG_DIR}/configure --prefix=${BUILD_X86_DIR}
+# make -j2
+# make install
+
+# # X86编译安装gflags
+# CONFIGURE_X86_GFLAGS=${BUILD_X86_DIR}/gflags
+# if [ ! -d ${CONFIGURE_X86_GFLAGS} ];then
+#     mkdir -p ${CONFIGURE_X86_GFLAGS}
+# fi
+# cd ${CONFIGURE_X86_GFLAGS}
+# cmake ${GFLAGS_DIR} -DCMAKE_INSTALL_PREFIX=${BUILD_X86_DIR}
+# make -j2
+# make install
+
+# 编译主项目工程X86
+PROJECT_BUILD_DIR=${BUILD_X86_DIR}/main
+if [ ! -d ${PROJECT_BUILD_DIR} ];then
+    mkdir -p ${PROJECT_BUILD_DIR}
 fi
-cd ${GLOG_DIR}
-git submodule update --init
+cd ${PROJECT_BUILD_DIR}
+export BUILD_DIR=${BUILD_X86_DIR}
+cmake ${PROJECT_DIR}
+make
+
 
 
 
---
+++ src/tool/rot-server.cc
@@ -0,0 +1,6 @@
+#include <glog/logging.h>
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
